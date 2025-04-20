68805c90627bd7bf2149c4d84c812d8fb646a878
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-04-20 17:48:15 -0300

    feat: Add product data for catalog

---
+++ src/infra/data/products.ts
@@ -0,0 +1,146 @@
+export const products = [
+  {
+    id: 1,
+    category: "Table",
+    price: 399.99,
+    name: "Modern Dining Table",
+    image:
+      "https://images.unsplash.com/photo-1685644201646-9e836c398c92?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=...",
+  },
+  {
+    id: 2,
+    category: "Chair",
+    price: 129.99,
+    name: "Industrial Chair",
+    image:
+      "https://images.unsplash.com/photo-1680745840784-318be3708374?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 3,
+    category: "Lamp",
+    price: 69.99,
+    name: "LED Desk Lamp",
+    image:
+      "https://images.pexels.com/photos/5400775/pexels-photo-5400775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    category: "Sofa",
+    price: 999.99,
+    name: "Luxury Sofa Set",
+    image:
+      "https://images.unsplash.com/photo-1680503146454-0fe569cef4eb?q=80&w=2071&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 5,
+    category: "Table",
+    price: 279.99,
+    name: "Glass Table",
+    image:
+      "https://images.unsplash.com/photo-1523841790171-6189ac051a5c?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 6,
+    category: "Chair",
+    price: 119.99,
+    name: "Wooden Chair",
+    image:
+      "https://images.unsplash.com/photo-1676834574878-bd42fa272eda?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 7,
+    category: "Lamp",
+    price: 89.99,
+    name: "Floor Lamp",
+    image:
+      "https://images.unsplash.com/photo-1675767528183-628d7e46ae59?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 8,
+    category: "Sofa",
+    price: 649.99,
+    name: "Comfy Sofa",
+    image:
+      "https://images.unsplash.com/photo-1703796932255-286a35d6e7d9?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 9,
+    category: "Table",
+    price: 349.99,
+    name: "Wooden Office Desk",
+    image:
+      "https://images.unsplash.com/photo-1620287341260-a9ecadfe7a17?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 10,
+    category: "Chair",
+    price: 99.99,
+    name: "Modern Office Chair",
+    image:
+      "https://images.pexels.com/photos/1957478/pexels-photo-1957478.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 11,
+    category: "Lamp",
+    price: 59.99,
+    name: "Table Desk Lamp",
+    image:
+      "https://images.unsplash.com/photo-1523380262778-076eb862d38f?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 12,
+    category: "Sofa",
+    price: 1299.99,
+    name: "Premium Leather Sofa",
+    image:
+      "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?q=80&w=2071&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 13,
+    category: "Table",
+    price: 319.99,
+    name: "Dining Table",
+    image:
+      "https://cdn.pixabay.com/photo/2021/11/19/07/45/interior-design-6808303_1280.jpg",
+  },
+  {
+    id: 14,
+    category: "Chair",
+    price: 109.99,
+    name: "Dining Chair",
+    image:
+      "https://images.unsplash.com/photo-1602872030490-4a484a7b3ba6?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 15,
+    category: "Sofa",
+    price: 799.99,
+    name: "Velvet Couch",
+    image:
+      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=2070&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 16,
+    category: "Table",
+    price: 269.99,
+    name: "Marble Side Table",
+    image:
+      "https://images.unsplash.com/photo-1681997963595-5e462b76d19c?q=80&w=1964&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 17,
+    category: "Chair",
+    price: 159.99,
+    name: "Leather Executive Chair",
+    image:
+      "https://images.unsplash.com/photo-1627451866896-58a8a43ea428?q=80&w=1931&auto=format&fit=crop&ixlib=...",
+  },
+  {
+    id: 18,
+    category: "Sofa",
+    price: 999.99,
+    name: "Cotton Sofa Set",
+    image:
+      "https://images.unsplash.com/photo-1653251307042-c5821df9d527?q=80&w=1974&auto=format&fit=crop&ixlib=...",
+  },
+];
