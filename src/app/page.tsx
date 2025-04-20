017e7ad33fa62e85b767d2e391096a7239fcee62
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-04-20 18:23:00 -0300

    refactor: Update Home component to use Promise for searchParams and simplify props structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { ProductPage } from "@components/product/product-page";
 
-export interface HomeProps {
-  searchParams: {
-    category: string;
-  };
+type SearchParamsType = Record<string, string | undefined>;
+
+interface HomeProps {
+  searchParams?: Promise<SearchParamsType>;
 }
 
-export default function Home(props: HomeProps) {
-  const { searchParams } = props;
-  const category = searchParams?.category || "";
-
+export default async function Home({ searchParams }: HomeProps) {
+  const resolvedSearchParams = await searchParams;
+  const category = resolvedSearchParams?.category ?? "";
   return <ProductPage category={category} />;
 }
