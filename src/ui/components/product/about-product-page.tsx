a98b2b0293476015abf2b4583a8e707a8559b38f
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-04-20 17:51:57 -0300

    feat: Add AboutProductPage component

---
+++ src/ui/components/product/about-product-page.tsx
@@ -0,0 +1,13 @@
+export function AboutProductPage() {
+  return (
+    <section className="flex flex-col gap-4 items-center">
+      <h1 className="text-5xl font-semibold">Our Product List</h1>
+      <p className="text-gray-500 max-w-3xl text-center">
+        Discover our curated selection of furniture pieces, each thoughtfully
+        chosen to bring comfort and style to your home. From inviting chairs and
+        cozy sofas to practical tables and decorative lamps, we offer a diverse
+        range of products to enhance any living space.
+      </p>
+    </section>
+  );
+}
